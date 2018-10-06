0392f3ff06a60db4060820f51838b78ec789a40c
areeshaFatima <areeshaf7@gmail.com>
2018-10-06 20:31:29 +0530

    Added TextFields for better UI and printing in Log

--- MyGreatApp/MygreatApp/app/src/main/java/com/example/hp/mygreatapp/MainActivity.java
+++ MyGreatApp/MygreatApp/app/src/main/java/com/example/hp/mygreatapp/MainActivity.java
@@ -2,8 +2,19 @@ package com.example.hp.mygreatapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void clickFunction(View view){
+        EditText name=(EditText) findViewById(R.id.editTextName);
+        EditText pass=(EditText) findViewById(R.id.editTextPassword);
+        Log.i("Info",   "Button Pressed");
+        Log.i("Values",name.getText().toString());
+        Log.i("Values",pass.getText().toString());
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
