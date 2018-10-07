c6ad0fc491ab6140535201383ef385dbca8c077b
areeshaFatima <areeshaf7@gmail.com>
2018-10-08 02:52:24 +0530

    updated

--- CurrencyConvertor/app/src/main/java/com/example/hp/currencyconvertor/MainActivity.java
+++ CurrencyConvertor/app/src/main/java/com/example/hp/currencyconvertor/MainActivity.java
@@ -11,21 +11,30 @@ public class MainActivity extends AppCompatActivity {
 
     public void clickfun(View view){
         EditText et=(EditText)findViewById(R.id.editText);
+
        String value= et.getText().toString();
-        Log.i("Info","Button pressed");
-        Log.i("Info",et.getText().toString());
-        int num=Integer.parseInt(value);
-        double amount=num*73.77;
-        String amountd=String.format("%.2f",amount);
-        Toast.makeText(this, "$"+et.getText().toString()+" is equal to Rps."+amountd, Toast.LENGTH_LONG).show();
+        if(value.isEmpty()){
+            Toast.makeText(this, "Please enter an amount!", Toast.LENGTH_SHORT).show();
+        }else {
+            Log.i("Info", "Button pressed");
+            Log.i("Info", et.getText().toString());
+            int num = Integer.parseInt(value);
+            double amount = num * 73.77;
+            String amountd = String.format("%.2f", amount);
+            Toast.makeText(this, "$" + et.getText().toString() + " is equal to Rps." + amountd, Toast.LENGTH_LONG).show();
+        }
     }
     public void clickFun(View v){
         EditText et=(EditText)findViewById(R.id.editText4);
         String amts=et.getText().toString();
-        Double amtd=Double.parseDouble(amts);
-        Double amnt=amtd/73.77;
-        String amnts=String.format("%.2f",amnt);
-        Toast.makeText(this, "Rps. "+amts+" is equal to $"+amnts, Toast.LENGTH_LONG).show();
+        if(amts.isEmpty()){
+            Toast.makeText(this, "Please enter an amount!", Toast.LENGTH_SHORT).show();
+        }else {
+            Double amtd = Double.parseDouble(amts);
+            Double amnt = amtd / 73.77;
+            String amnts = String.format("%.2f", amnt);
+            Toast.makeText(this, "Rps. " + amts + " is equal to $" + amnts, Toast.LENGTH_LONG).show();
+        }
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
